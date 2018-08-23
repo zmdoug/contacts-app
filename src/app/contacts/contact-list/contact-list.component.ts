1e898509926f82465ab2658d8a9c9f6fca33a515
Douglas M Z <dmz@rede.local>
2018-08-23 17:11:42 -0300

    Criando a lógica do app com o component contact-list

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,15 +1,89 @@
+// Adicionando a logica do APP
+
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact'; // Importando classe Contact
+import { ContactService } from '../contact.service'; // Importando requisições
+import { ContactDetailsComponent } from '../contact-details/contact-details.component'; // Template para exibir os detalhes do contato
 
 @Component({
-  selector: 'app-contact-list',
+  selector: 'contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
+  providers: [ContactService]
 })
+
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  contacts: Contact[]
+  selectedContact: Contact
+
+  constructor(private contactService: ContactService) { }
 
   ngOnInit() {
+  	// Recuperar lista completa de contatos
+  	// Armazena localmente para modificar dinamicamente sem necessidade de realizar requisições HTTP extras para a API.
+     this.contactService
+      .getContacts()
+      .then((contacts: Contact[]) => {
+        this.contacts = contacts.map((contact) => {
+          if (!contact.phone) {
+            contact.phone = {
+              mobile: '',
+              work: ''
+            }
+          }
+          return contact;
+        });
+      });
   }
 
+  private getIndexOfContact = (contactId: String) => {
+    return this.contacts.findIndex((contact) => {
+      return contact._id === contactId;
+    });
+  }
+	// Criando as funções do app
+
+	// Ao selecionar um contato
+  selectContact(contact: Contact) {
+    this.selectedContact = contact
+  }
+  // Ao criar um novo contato
+  createNewContact() {
+    var contact: Contact = {
+      name: '',
+      email: '',
+      phone: {
+        work: '',
+        mobile: ''
+      }
+    };
+
+    // Seleciona o contato recém criado
+    this.selectContact(contact);
+  }
+  // Ao deletar um contato
+  deleteContact = (contactId: String) => {
+    var idx = this.getIndexOfContact(contactId);
+    if (idx !== -1) {
+      this.contacts.splice(idx, 1); // splice = remover do array
+      this.selectContact(null);
+    }
+    return this.contacts;
+  }
+
+  addContact = (contact: Contact) => {
+    this.contacts.push(contact);
+    this.selectContact(contact);
+    return this.contacts;
+  }
+
+  updateContact = (contact: Contact) => {
+    var idx = this.getIndexOfContact(contact._id);
+    if (idx !== -1) {
+      this.contacts[idx] = contact;
+      this.selectContact(contact);
+    }
+    return this.contacts;
+  }
 }
